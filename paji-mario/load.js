73bc2e0421532556f1c43f02684c698090ce7ea3
oms733411-commits <oms733411@gmail.com>
2026-09-23 22:45:58 +0530

    Delete Paji Mario project: paji-mario/load.js

--- paji-mario/load.js
+++
@@ -1,7 +0,0 @@
-/* Load.js - maps are already embedded in maps.js for this Paji Mario build. */
-function startLoadingMaps() { return; }
-function passivelyLoadMap() { return; }
-function setNextLevelArr(arr) {
-  if(arr[1]++ == 4) { ++arr[0]; arr[1] = 1; }
-  return arr;
-}
